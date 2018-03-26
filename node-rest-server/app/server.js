657388158ce2b5186471926b4223c3e6870b78f3
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-03-26 16:12:26 -0700

    [prog] passes first tests

--- node-rest-server/app/server.js
+++ node-rest-server/app/server.js
@@ -5,7 +5,8 @@ var IP = '127.0.0.1';
 
 var server = http.createServer(function(request, response) {
   console.log('Serving ' + request.method + ' request to url: ' + request.url);
-  res.end('Hello, World!');
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end(JSON.stringify('Hello, World!'));
 });
 
 console.log('Listening on http://' + IP + ':' + PORT);
