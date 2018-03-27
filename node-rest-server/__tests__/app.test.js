ed9c39fc6b72ad052eb9324e4920c7cd4d0a3848
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-03-26 20:14:09 -0700

    [prog] passes additional tests

--- node-rest-server/__tests__/app.test.js
+++ node-rest-server/__tests__/app.test.js
@@ -4,21 +4,42 @@ describe('My RESTful API', () => {
   const CITY_ENDPOINT = 'http://localhost:1337/api/location/city';
   const RESTAURANT_ENDPOINT = 'http://localhost:1337/api/location/restaurant';
 
-  test('should respond with stringified JSON.', () => {
+  test('should respond to requests to /api/locations/cities with stringified JSON.', () => {
     request(CITY_ENDPOINT, function(error, response, body) {
       expect(() => JSON.parse(body)).not.toThrow();
     });
   });
 
-  test('should respond with stringified JSON.', () => {
+  test('should respond to requests to /api/locations/restaurants with stringified JSON.', () => {
     request(RESTAURANT_ENDPOINT, function(error, response, body) {
       expect(() => JSON.parse(body)).not.toThrow();
     });
   });
 
-  // test('should send back an object.', () => {});
-  // test('should respond to GET requests to /api/locations/cities with a 200 status code.', () => {});
-  // test('should respond to GET requests to /api/locations/cities with an object containing a `data` array of all the cities.', () => {});
+  test('should respond to requests to /api/locations/cities with a parsable stringified object.', () => {
+    request(CITY_ENDPOINT, function(error, response, body) {
+      expect(typeof JSON.parse(body)).toBe('object');
+    });
+  });
+
+  test('should respond to requests to /api/locations/restaurants with a parsable stringified object.', () => {
+    request(RESTAURANT_ENDPOINT, function(error, response, body) {
+      expect(typeof JSON.parse(body)).toBe('object');
+    });
+  });
+
+  test('should respond to GET requests to /api/locations/cities with a 200 status code.', () => {
+    request(CITY_ENDPOINT, function(error, response, body) {
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  test('should respond to GET requests to /api/locations/cities with an object containing a `data` array of all the cities.', () => {
+    request(CITY_ENDPOINT, function(error, response, body) {
+      var data = JSON.parse(body);
+      expect(Array.isArray(data)).toBe(true);
+    });
+  });
   // test('should respond to POST requests to /api/locations/cities with a 201 status code if sending a city object that contains all required properties.', () => {});
   // test('should respond to POST requests to /api/locations/cities with a 400 status code if sending a city object that DOES NOT contain all required properties.', () => {});
   // test('should respond to POST requests to /api/locations/cities with the city object that was created.', () => {});
--- node-rest-server/app/server.js
+++ node-rest-server/app/server.js
@@ -4,9 +4,9 @@ var PORT = 1337;
 var IP = '127.0.0.1';
 
 var server = http.createServer(function(request, response) {
-  console.log('Serving ' + request.method + ' request to url: ' + request.url);
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end(JSON.stringify('Hello, World!'));
+  console.log('Serving ' + request.method + ' request to ' + request.url);
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify({}));
 });
 
 console.log('Listening on http://' + IP + ':' + PORT);
